c1a2ea95185644770c952560c43cf4150acaf451
Dieter Bergmann <dieterbergmann@arcor.de>
2023-12-28 08:26:21 +0100

    Initial Commit

---
+++ GitHubExerciseLabtop/GitHubExerciseLabtopApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubExerciseLabtopApp.swift
+//  GitHubExerciseLabtop
+//
+//  Created by Dieter Bergmann on 28.12.23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubExerciseLabtopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
